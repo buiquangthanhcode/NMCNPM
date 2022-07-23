02f91e78e94768d2aef465a01553a42032c4630f
Thong <20127634@student.hcmus.edu.vn>
2022-07-23 20:24:09 +0700

    update Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,30 +1,24 @@
 import "./navbar.css";
 import "./navbar.css";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import React from "react";
 const Navbar = () => {
   const { user } = useContext(AuthContext); // check user who accessing webNavbar.jsxsite
   console.log("Navbar")
   console.log(user)
   return (
-    <div className="navbar">
-      <div className="navContainer">
-        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-          <span className="logo">Hotel Booking</span>
-        </Link >
-        {user ? user.username : ( // can link to here user profile
-            <div className="navItems">
-              <Link to="/login">
-              <button className="navButton">Đăng nhập</button>
-              </Link>
-              <Link to="/register">
-              <button className="navButton">Đăng kí</button>
-              </Link>
-            </div>  
-        )}
+    <div className="Header">
+      <div className="navbar">
+        <div className="navContainer">
+          <span className="logo">Book4Fun.com</span>
+          <div className="navItems">
+            <Link to="/login"><button className="navButton">Đăng nhập/ Đăng ký</button></Link>
+          </div>
+        </div>
       </div>
-    </div>
+      </div>
   );
 };
 
