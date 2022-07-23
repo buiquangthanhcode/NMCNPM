81eb0c5967cd2a5278e4dbe0025e09df64c613ff
Thong <20127634@student.hcmus.edu.vn>
2022-07-23 19:36:33 +0700

    update FeaturedProperties

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,12 +1,35 @@
 import "./featuredProperties.css";
 import useFetch from "../../hook/useFetch";
+import SwiperCore,{ Navigation, Pagination, Scrollbar, A11y, Thumbs } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 const FeaturedProperties = () => {
-  const { data, loading, error } = useFetch("http://localhost:8800/api/hotels?featured=true&limit=4");
+  const { data, loading, error } = useFetch("http://localhost:8800/api/hotels?featured=true&limit=15");
   return (
     <div className="fp">
       {loading?"Loading":<>
+      <Swiper
+      // install Swiper modules
+                modules={[Navigation, Thumbs]}
+                speed={1300}
+                grabCursor={true}
+                spaceBetween={10}
+                slidesPerView={4}
+                
+                navigation={true}
+
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
       {data.map(item=>(
-      <div className="fpItem" key={item._id}>
+      <SwiperSlide className="fpItem" key={item._id}>
        
         <img
           // src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/88905224.jpg?k=c8267f8981ea21b656ac1589b6fc78b0711261c14d237c4d326a404446111254&o=&hp=1"
@@ -21,9 +44,10 @@ const FeaturedProperties = () => {
           <button>{item.rating}</button>
           <span>Excellent</span>
         </div>}
-      </div>
+      </SwiperSlide>
        ))
        }
+       </Swiper>
       </>}
     </div>
   );
