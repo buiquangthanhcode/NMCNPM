2313d8c69a586d3683b0c85674127ce41d589157
Tran Gia Lac <dauphongdeptrai167@gmail.com>
2022-08-05 02:58:14 +0700

    hotelrep.js

---
+++ test-admin/hotelrep.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { List, Datagrid, TextField, EmailField, UrlField } from 'react-admin';
+
+export const UserList = () => (
+    <List>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
