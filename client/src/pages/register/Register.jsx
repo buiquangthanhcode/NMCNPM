dd634d608c2b4e32b0a0846a324c134e09324e1a
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2022-08-07 20:10:29 +0700

    UPDATE FRONTEND + BACKEND, CREATE REPRESENTATIVE

---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,100 @@
+import './register.css';
+import axios from 'axios';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    console.log(e.target.value);
+  };
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    console.log(credentials);
+    try {
+      const res = await axios.post(
+        'http://localhost:8800/api/auth/register',
+        credentials
+      );
+      navigate('/');
+    } catch (err) {
+      console.log('Loi');
+    }
+  };
+
+  return (
+    <div class="main-wrap">
+      <div class="form-wrap">
+        <h1>
+          SIGN <span>UP</span>
+        </h1>
+        <h6>Welcome to Hotel Booking</h6>
+        <form class="form">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          <br></br>
+          <input
+            type="username"
+            name="username"
+            id="username"
+            placeholder="Username"
+            onChange={handleChange}
+          />
+          <br></br>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <br></br>
+          <input
+            type="password"
+            name="cpass"
+            id="cpass"
+            placeholder="Confirm Password"
+          />
+          <br></br>
+          <button onClick={handleClick} type="submit" class="form-btn">
+            SIGN UP
+          </button>
+        </form>
+        <span class="sign-up-with">Or sign up with</span>
+        <div class="social-logo">
+          <a href="https://vnexpress.net/" title="Facebook">
+            <img
+              src="https://github.com/surajaswal-dev/signin-signup-pages/blob/main/signup-form-0/images/fb.png?raw=true"
+              alt="Facebook"
+            />
+          </a>
+          <a href="https://vnexpress.net/" title="Google">
+            <img
+              src="https://raw.githubusercontent.com/surajaswal-dev/signin-signup-pages/main/signup-form-0/images/google.png"
+              alt="Google"
+            />
+          </a>
+          <a href="https://vnexpress.net/" title="Twitter">
+            <img
+              src="https://raw.githubusercontent.com/surajaswal-dev/signin-signup-pages/main/signup-form-0/images/twitter.png"
+              alt="Twitter"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ client/src/pages/userProfile/UserProfile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import './userProfile.css';
+import Footer from '../../components/footer/Footer';
+import HeaderUser from '../../components/headerUser/HeaderUser';
+export default function UserProfile() {
+  return (
+    <div>
+      <HeaderUser tk={1} ks={0} yt={0} />
+      <div className="functionItemContent">
+        <div className="functionItemContentItem">
+          <div className="contentItem">
+            <div className="avatarUser">
+              <img
+                src="https://laban.edu.vn/wp-content/uploads/2021/01/Profile_avatar_placeholder_large.png"
+                alt="avatar"
+                className="avatarUserr"
+              />
+            </div>
+            <div className="infoUser">
+              <input className="inputUser" type="text" placeholder="Họ tên" />
+              <input className="inputUser" type="text" placeholder="Email" />
+              <input
+                className="inputUser"
+                type="text"
+                placeholder="Số điện thoại"
+              />
+              <input className="inputUser" type="text" placeholder="Địa chỉ" />
+              <div className="Save">
+                <button className="saveUser">Lưu lại</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="homeContainer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
